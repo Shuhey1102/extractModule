d44de119519fa128ec2e531488f9dcac486aed54
梶原修平 <shuhei_kajiwara@qualica.co.jp>
2024-11-20 19:02:04 +0900

    update search SQL

--- extractModule_withSQL.py
+++ extractModule_withSQL.py
@@ -3,6 +3,7 @@ import csv
 from concurrent.futures import ProcessPoolExecutor
 import extractSQL_lib
 import datetime
+import re
 
 #current datetime
 dt_now = datetime.datetime.now()
@@ -21,6 +22,13 @@ def search_files_for_keywords_in_folder(folder_path, keywords):
     # 結果を格納するリスト
     results = []
 
+    daoFunction = ["setDiconDir","pagerSelectSQL","selectSQL","updateSQL","selectOrderSQL","selectSQLReplace","replaceSqlKpiTableId","getDiconPath","removeBlankRow","setLocator"]
+ 
+    # 正規表現の作成
+    dao_pattern = "|".join(re.escape(func) for func in daoFunction)
+    sql_pattern = "|".join(re.escape(sql) for sql in keywords)
+    regex_pattern = rf"({dao_pattern})\(\"({sql_pattern})\".*?\);"
+
     # フォルダ内の全てのファイルを走査
     for dirpath, _, filenames in os.walk(folder_path):
         for filename in filenames:
@@ -30,11 +38,13 @@ def search_files_for_keywords_in_folder(folder_path, keywords):
                 # ファイルを行ごとに読み込む             
                 with open(file_path, 'r', encoding='utf-8') as file:
                     for line_number, line in enumerate(file, start=1):  # 行番号をカウント
+
                         # ファイルの中身に対してキーワードを検索
                         for keyword in keywords:
                             if keyword in line:
                                 # 一致した場合、結果に追加
                                 results.append([filename, dirpath, keyword, line.strip(), line_number])
+
             except Exception as e:
                 print(f"エラー: {file_path} を読み込む際に問題が発生しました: {e}")
 
@@ -66,14 +76,14 @@ def main():
     # キーワードリストを直接指定
     keywords = extractSQL_lib.runExtractSQL(sql_dir,"<component name=\".*\"")
 
-    while(True):
-        isEnd = input("他に追加すべき検索キーワードがありますか？(y:n): ")
-        if isEnd == "n":
-            break        
-        key = input("検索キーワード: ")
-        if(str.strip(key) != ""):
-            keywords.append(key)
-            print(f"{key}を追加しました")
+    # while(True):
+    #     isEnd = input("他に追加すべき検索キーワードがありますか？(y:n): ")
+    #     if isEnd == "n":
+    #         break        
+    #     key = input("検索キーワード: ")
+    #     if(str.strip(key) != ""):
+    #         keywords.append(key)
+    #         print(f"{key}を追加しました")
 
     root_dir = input("検索対象のフォルダ: ")
 
